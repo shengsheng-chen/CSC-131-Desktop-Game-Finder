972b1fc79d745bb57a2fcc059fcf6d4efb9b4fa7
shengsheng-chen <sheng-changchen@csus.edu>
2024-11-22 17:15:02 -0800

    very nice

---
+++ lab02/src/Hello.java
@@ -0,0 +1,20 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Git!");
+		System.out.println("yang - This line was added by Dr. Yang.");
+		// Add a new line with your information
+		System.out.println("303902677 - This line was added by Ikki Ee Herng Last name.");
+		System.out.println("303372394 - This line was added by Sheng-chang Chen.");
+		System.out.println("303189770 - This line was added by Mohammed Bader.");
+		System.out.println("3028422878 - This line was added by Alan Her.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
+
+}
+
+}
