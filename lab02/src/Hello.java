9afd1d86bc6200132eee000dcc5429afcd93d58d
shengsheng-chen <sheng-changchen@csus.edu>
2024-11-26 17:18:52 -0800

    cheese

--- lab02/src/Hello.java
+++ lab02/src/Hello.java
@@ -2,6 +2,7 @@
 public class Hello {
 
 	public static void main(String[] args) {
+		// I love cheeseburger
 		System.out.println("Welcome to Git!");
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
