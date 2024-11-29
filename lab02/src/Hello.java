2548f2b40eabe1c75a05bd3ed3d5324c040b0ec2
Jonathan <Jonathan@DESKTOP-G84M254.attlocal.net>
2024-11-29 14:56:44 -0800

    help

--- lab02/src/Hello.java
+++ lab02/src/Hello.java
@@ -12,7 +12,7 @@ public class Hello {
 		System.out.println("3028422878 - This line was added by Alan Her.");
 		System.out.println("302299231 - This line was added by Ben Church.");
 		System.out.println("302813914 - This line was added by Shivjyot Brar.");
-		System.out.println("303050007 -  This line was added by Jonathan Herman.");
+		System.out.println("303050007 - This line was added by Jonathan Herman.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 
