1543232bba0c581ab12e984e036112eb4971f48a
iceyatom <kk6uzh2@gmail.com>
2024-11-30 20:48:02 +0000

    this is a test

---
+++ test/test.java
@@ -0,0 +1,13 @@
+class Test{
+    public static void main(String [] args){
+
+        System.out.println("Trying to figure out how this works.");
+
+        for(int i = 0; i < 100; i++){
+            System.out.print(" " + i + " ");
+
+        }
+
+    }
+
+}
