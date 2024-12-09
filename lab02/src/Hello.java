3e58ed5826e4c2b522ad0c415376a92aa52391e7
abambhroliya <abambhroliya@UL-ARC1003-2080.saclink.csus.edu>
2024-12-08 18:00:01 -0800

    Done

--- lab02/src/Hello.java
+++ lab02/src/Hello.java
@@ -13,8 +13,9 @@ public class Hello {
 		System.out.println("302299231 - This line was added by Ben Church.");
 		System.out.println("302813914 - This line was added by Shivjyot Brar.");
 		System.out.println("303050007 - This line was added by Jonathan Herman.");
+		System.out.println("303335591 - This line was added by Ayush Bambhroliya.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		//Test
 
 }
 
